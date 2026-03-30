1d00ae0decf85ef88cbe023365bc60083331f624
patriciaamanda <patricia24@jiu.ac>
2026-03-31 00:39:49 +0700

    Update AddItem.java

--- AddItem.java
+++ AddItem.java
@@ -1,13 +1,13 @@
 import java.util.ArrayList;
 
 public class AddItem {
-   public static void addItem(ArrayList<MenuItem> orderList, MenuItem item) {
+ public static void addItem(ArrayList<MenuItem> orderList, MenuItem item) {
 
-     if (item == null) {
-        System.out.println("Item not valid");
-     } else {
-        orderList.add(item);
-        System.out.println(item.getName() + " added to order");
-        }
+    if (item == null) {
+      System.out.println("Item not valid");
+    } else {
+      orderList.add(item);
+      System.out.println(item.getName() + " added to order");
     }
+  }
 }
