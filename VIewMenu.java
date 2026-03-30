01d942cf6e29efb9551bcfbe819da6af23e68a36
dastinyuasiva <dastin24@jiu.ac>
2026-03-31 01:07:23 +0700

    Update ViewMenu.java

--- VIewMenu.java
+++ VIewMenu.java
@@ -1,2 +1,19 @@
-java.util.ArrayList;
-ya
+import java.util.ArrayList;
+
+public class ViewMenu {
+
+    public static void showMenu(ArrayList<MenuItem> menuList) {
+
+        System.out.println("=== MENU LIST ===");
+
+        for (MenuItem item : menuList) {
+            System.out.println(
+                    item.getName() +
+                            " | Category: " + item.getCategory() +
+                            " | Price: " + item.getPrice()
+            );
+        }
+
+        System.out.println("=================");
+    }
+}
