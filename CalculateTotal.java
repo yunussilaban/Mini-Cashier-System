5129148db0cb88d19b12f5b207aeb0e77f246d98
jasonjuni37-ux <jasonjuni37@gmail.com>
2026-03-31 07:51:31 +0700

    Adding CalculateTotal

--- CalculateTotal.java
+++ CalculateTotal.java
@@ -1,1 +1,16 @@
-test
+import java.util.ArrayList;
+
+public class CalculateTotal {
+
+    public static void calculate(ArrayList<MenuItem> orderList) {
+
+        double total = 0;
+
+        for (MenuItem item : orderList) {
+            total = total + item.getPrice();
+        }
+
+        System.out.println("Total price: " + total);
+    }
+
+}
