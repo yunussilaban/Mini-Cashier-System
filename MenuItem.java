1ea8d0fb50712ca592e48f68357c672b6a2dc779
yunussilaban <silabanyunus@gmail.com>
2026-03-31 07:14:52 +0700

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -1,1 +1,25 @@
+public class MenuItem {
 
+    String name;
+    String category;
+    double price;
+
+    public MenuItem(String name, String category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
